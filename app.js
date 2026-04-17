396ae1eff9557f4abd74ef348f1f0d8517b0fb78
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-17 10:39:41 +0200

    feat(server): initialize Express server

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("THE SERVER LISTENING ON 3000 .... "));
