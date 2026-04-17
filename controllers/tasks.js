5b484a2029f502a48a3507d397663aabf9a49f45
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-17 11:02:08 +0200

    feat(tasks): implement task CRUD operations
    - Added create, get, update, delete task controllers
    - Exposed new routes for task operations
    - Updated router to include new task endpoints

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,23 @@ const getAllTasks = (req, res) => {
   res.send("ALL ITEMS");
 };
 
+const createTasks = (req, res) => {
+  res.send("create new task");
+};
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+const updateTasks = (req, res) => {
+  res.send("update existing task");
+};
+const deleteTasks = (req, res) => {
+  res.send("task is deleted");
+};
+
 module.exports = {
   getAllTasks,
+  deleteTasks,
+  updateTasks,
+  createTasks,
+  getTask,
 };
--- routers/tasks.js
+++ routers/tasks.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { getAllTasks } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  createTasks,
+  deleteTasks,
+  getTask,
+  updateTasks,
+} = require("../controllers/tasks");
 const router = express.Router();
 
 router.get("/", getAllTasks);
+router.post("/", createTasks);
+router.get("/:id", getTask);
+router.patch("/:id", updateTasks);
+router.get("/:id", deleteTasks);
 
 module.exports = router;
