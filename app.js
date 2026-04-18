77da9814b00c93a19a28ba64122a6ccf1714b3e8
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-18 07:39:23 +0200

    feat(db): Add MongoDB connection
    - Establish MongoDB connection via Mongoose
    - Load DB URL from environment variables
    - Update `mongodb` and `mongoose` dependencies

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const tasksRouter = require("./routers/tasks");
+// const mongodbConnect = require("./db/connection");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.use("/api/v1/tasks", tasksRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log("THE SERVER LISTENING ON 3000 .... "));
+// const PORT = 3000;
+mongoose.connect(`${process.env.Mongo_URL}`, () => {
+  console.log("connection establish");
+
+  app.listen(3000);
+});
+// app.listen(PORT, () => console.log("THE SERVER LISTENING ON 3000 .... "));
---
+++ db/connection.js
@@ -0,0 +1,20 @@
+// require("dotenv");
+
+// const mongoose = require("mongoose");
+// // const mongodb = require("mongodb");
+// const mongodbClient = mongodb.MongoClient;
+// const connectionString =
+//   process.env.CONNECTION_URL ||
+//   "mongodb+srv://mostafa:Mongo2930206@cluster0.embynly.mongodb.net/?appName=Cluster0";
+
+// const mongodbConnect = (callback) => {
+//   return mongodbClient
+//     .connect(connectionString)
+//     .then((result) => {
+//       console.log("CONNETCED TO DATABASE");
+//       callback(result);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// module.exports = mongodbConnect;
