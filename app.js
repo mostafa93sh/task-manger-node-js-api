23dc7636889d60a18081be90c9a00e2766dd0592
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-18 08:48:10 +0200

    feat: Add JSON request body parsing
    - Enable JSON request body parsing
    - Refactor DB connection to use promises
    - Add error handling for DB connection

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const tasksRouter = require("./routers/tasks");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+app.use(express.json());
+
 app.use("/api/v1/tasks", tasksRouter);
 
 // const PORT = 3000;
-mongoose.connect(`${process.env.Mongo_URL}`, () => {
-  console.log("connection establish");
-
-  app.listen(3000);
-});
+mongoose
+  .connect(`${process.env.Mongo_URL}`)
+  .then(() => {
+    console.log("CONNECTION ESTABLISH");
+  })
+  .catch((err) => console.log(err));
+app.listen(3000);
 // app.listen(PORT, () => console.log("THE SERVER LISTENING ON 3000 .... "));
